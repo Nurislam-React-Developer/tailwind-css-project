bd59e0cd7656d3a5ea78c40729c24cc073e712a5
github20243 <98372q@gmailc.com>
2025-01-23 20:39:54 -0800

    Сделал стили красивым в компоненте herder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<header className='flex justify-around items-center bg-slate-400'>
+		<header className='flex justify-around items-center bg-gradient-to-r from-slate-400 to-slate-600 p-4 shadow-lg transition-all duration-300'>
 			<img
-				src="src/assets/img/logo.png"
+				src='src/assets/img/logo.png'
 				alt='logo'
-				className='w-[70px] h-[70px]'
+				className='w-[70px] h-[70px] transition-transform duration-300 hover:scale-110'
 			/>
 			<section>
-				<ul className='flex gap-20 text-cyan-300 cursor-wait'>
-					<li className='hover:border-b-2 transition-all'>Home</li>
-					<li className='hover:border-b-2 transition-all'>About</li>
-					<li className='hover:border-b-2 transition-all'>Products</li>
+				<ul className='flex gap-20 text-cyan-300 cursor-pointer'>
+					{['Home', 'About', 'Products'].map((item) => (
+						<li
+							key={item}
+							className='relative hover:text-white cursor-pointer group'
+						>
+							{item}
+							<span className='absolute bottom-0 left-0 w-0 h-1 bg-cyan-300 transition-all duration-300 group-hover:w-full'></span>
+						</li>
+					))}
 				</ul>
 			</section>
 		</header>
